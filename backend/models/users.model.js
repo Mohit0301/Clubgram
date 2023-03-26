02c82fc5d37bca6b79b54d259a5420c3cf0bb5cd
Mohit Panchariya <iammohitpanchariya@gmail.com>
2023-03-26 19:52:38 +0530

    Refactor user model
    Made modifications to make the users model more robust.

--- backend/models/users.model.js
+++ backend/models/users.model.js
@@ -4,38 +4,45 @@ const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
     user_name: {
-        type:String
+        type:String,
+        minlength: 4,
+        maxlength: 50,
+        required: [true, "User Name required."]
     },
     privileges:{
         type:Boolean,
         default:false,
     },
     roll_number:{
-        type:String,
-        default:"",
+        type:Number,
+        required: [true, "Roll Number is required."]
     },
     branch:{
         type:String,
-        default:"",
+        enum: {
+            values: ["CSE", "ECE", "EEE", "MEC", "CIV", "CHEM", "ARCH"],
+            message: "{VALUE} is not a supported branch."
+        },
     },
     year:{
-        type:String,
-        default:"",
+        type:Number,
+        enum: {
+            values: [1, 2, 3, 4],
+            message: "Year must be 1, 2, 3, or 4. {VALUE} is not a valid year."
+        }
     },
     profile_pic: {
         type:String,
-        
-      
     },
     email_id:{
         type:String,
+        required: [true, "Email ID is required."],
+        unique: true
     },
     password:{
         type: String,
+        required: [true, "Password is required."]
     }
-
-    
-  
  });
   
  const user = mongoose.model('User',userSchema);
